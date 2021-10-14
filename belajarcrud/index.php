2957de22f80ebc550fe5a96b4f6ac82b32dd2d7c
Desy-Ani <80753482+Desy-Ani@users.noreply.github.com>
2021-10-14 16:52:37 +0700

    Add files via upload

---
+++ belajarcrud/index.php
@@ -0,0 +1,22 @@
+<?php
+include 'functions.php';
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Welcome to the home page!</p>
+</div>
+<div>
+		<center>
+		<img src="php.jpg" alt=""class="banner" >
+		</center>
+	</div>
+	<div>
+		<center><h1 > Pengenalan PHP</h1></center>
+		<center><p>
+			PHP  (PHP: Hypertext Preprocessor) adalah sebuah bahasa pemrograman server side scripting yang bersifat open source.Sebagai sebuah scripting language, PHP menjalankan instruksi pemrograman saat proses runtime. Hasil dari instruksi tentu akan berbeda tergantung data yang diproses.PHP merupakan bahasa pemrograman server-side, maka script dari PHP nantinya akan diproses di server. Jenis server yang sering digunakan bersama dengan PHP antara lain Apache, Nginx, dan LiteSpeed. Selain itu, PHP juga merupakan bahasa pemrograman yang bersifat open source.
+		</p></center>
+	</div>
+<?=template_footer()?>
